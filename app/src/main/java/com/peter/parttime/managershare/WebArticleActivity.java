3b0fa3d94f0b563684e88760e7d581a0f0ec2e94
Peter <xiongjianfei@soundtooth.cn>
2016-04-06 10:48:00 +0800

    Adjust article content view layout

--- app/src/main/java/com/peter/parttime/managershare/WebArticleActivity.java
+++ app/src/main/java/com/peter/parttime/managershare/WebArticleActivity.java
@@ -67,13 +67,7 @@ public class WebArticleActivity extends Activity {
 
     private static final int SWIPE_RIGHT_DISTANCE = 300;
     private static final int SWIPE_RIGHT_VELOCITY = 100;
-
     private static final int MAX_ARTICLE_CACHE_COUNT = 200;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-//    private GoogleApiClient client;
 
     private static final void dbg(String msg) {
         ManagerShareActivity.dbg(msg);
@@ -119,9 +113,6 @@ public class WebArticleActivity extends Activity {
         mHandler = new UIHandler(this);
         mHandler.sendEmptyMessage(MSG_INIT_VIEW);
 
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-//        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     private String getDocumentCacheName(String url) {
@@ -148,40 +139,12 @@ public class WebArticleActivity extends Activity {
     public void onStart() {
         super.onStart();
 
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-//        client.connect();
-//        Action viewAction = Action.newAction(
-//                Action.TYPE_VIEW, // TODO: choose an action type.
-//                "WebArticle Page", // TODO: Define a title for the content shown.
-//                // TODO: If you have web page content that matches this app activity's content,
-//                // make sure this auto-generated web page URL is correct.
-//                // Otherwise, set the URL to null.
-//                Uri.parse("http://host/path"),
-//                // TODO: Make sure this auto-generated app URL is correct.
-//                Uri.parse("android-app://com.peter.parttime.managershare/http/host/path")
-//        );
-//        AppIndex.AppIndexApi.start(client, viewAction);
     }
 
     @Override
     public void onStop() {
         super.onStop();
 
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-//        Action viewAction = Action.newAction(
-//                Action.TYPE_VIEW, // TODO: choose an action type.
-//                "WebArticle Page", // TODO: Define a title for the content shown.
-//                // TODO: If you have web page content that matches this app activity's content,
-//                // make sure this auto-generated web page URL is correct.
-//                // Otherwise, set the URL to null.
-//                Uri.parse("http://host/path"),
-//                // TODO: Make sure this auto-generated app URL is correct.
-//                Uri.parse("android-app://com.peter.parttime.managershare/http/host/path")
-//        );
-//        AppIndex.AppIndexApi.end(client, viewAction);
-//        client.disconnect();
     }
 
     private class UIHandler extends Handler {
